14aef4b3ff246c549933e84c88f97fa47399dab3
Jacob Carlson <jatcar95@cs.washington.edu>
2016-01-30 15:06:06 -0800

    adding menu to view home page

--- BowandArrow/app/src/main/java/com/winfo/bowandarrow/ViewHome.java
+++ BowandArrow/app/src/main/java/com/winfo/bowandarrow/ViewHome.java
@@ -5,6 +5,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 public class ViewHome extends AppCompatActivity {
@@ -13,10 +15,17 @@ public class ViewHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.top_bar);
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.view_home_menu, menu);
+        return true;
     }
 
 }
