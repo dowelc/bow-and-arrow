3ed828c0c4c41ecdbbc855fca766a2ed6844e0cd
Colin Bartels <colin.0011@hotmail.com>
2016-01-30 15:48:00 -0800

    working on editHome

--- BowandArrow/app/src/main/java/com/winfo/bowandarrow/EditHomeActivity.java
+++ BowandArrow/app/src/main/java/com/winfo/bowandarrow/EditHomeActivity.java
@@ -1,6 +1,8 @@
 package com.winfo.bowandarrow;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -16,15 +18,8 @@ public class EditHomeActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Intent intent = getIntent();
+        Parcelable myParcelableObject = (Parcelable) intent.getParcelableExtra("com.winfo.bowandarrow.Home");
     }
 
 }
