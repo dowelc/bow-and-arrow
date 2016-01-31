2b960e945ad8c21742424f7e4304d382fdae4e8d
Colin Bartels <colin.0011@hotmail.com>
2016-01-30 16:02:22 -0800

    asf

--- BowandArrow/app/src/main/java/com/winfo/bowandarrow/MainActivity.java
+++ BowandArrow/app/src/main/java/com/winfo/bowandarrow/MainActivity.java
@@ -19,15 +19,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         FloatingActionButton hackToViewHomeBut = (FloatingActionButton) findViewById(R.id.hack_view_home);
         hackToViewHomeBut.setOnClickListener(new View.OnClickListener() {
             @Override
