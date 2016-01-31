27860c0f9bb858abe98dce01dde2a5d4aee56af0
Colin Bartels <colin.0011@hotmail.com>
2016-01-30 16:45:27 -0800

    edit home

--- BowandArrow/app/src/main/java/com/winfo/bowandarrow/EditHomeActivity.java
+++ BowandArrow/app/src/main/java/com/winfo/bowandarrow/EditHomeActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class EditHomeActivity extends AppCompatActivity {
 
@@ -19,7 +20,33 @@ public class EditHomeActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Intent intent = getIntent();
-        Parcelable myParcelableObject = (Parcelable) intent.getParcelableExtra("com.winfo.bowandarrow.Home");
+        Home home = getIntent().getExtras().getParcelable("com.winfo.bowandarrow.Home");
+
+        EditText address = (EditText) findViewById(R.id.address);
+        EditText nickname = (EditText) findViewById(R.id.nickname);
+        EditText bathrooms = (EditText) findViewById(R.id.bathrooms);
+        EditText bedrooms = (EditText) findViewById(R.id.bedrooms);
+        EditText price = (EditText) findViewById(R.id.price);
+
+        if (home.getAddress() != null) {
+            address.setText(home.getAddress());
+        }
+
+        if (home.getNickname() != null) {
+            nickname.setText(home.getNickname());
+        }
+
+        if (home.getBathrooms() != null) {
+            bathrooms.setText(home.getBathrooms());
+        }
+
+        if (home.getBedrooms() != null) {
+            bedrooms.setText(home.getBedrooms());
+        }
+
+        if (home.getPrice() != null) {
+            price.setText(home.getPrice());
+        }
     }
 
 }
